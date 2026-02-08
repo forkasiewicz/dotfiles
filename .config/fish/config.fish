3842abe7c5b399d6cf2586902d42972a5b449c9a
forkasiewicz <111523660+forkasiewicz@users.noreply.github.com>
2026-02-08 11:05:46 +0100

    mac fish keybinds

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,21 @@
 if status is-interactive
 # Commands to run in interactive sessions can go here
 end
+
+set -g fish_greeting
+
+# if macos
+if test (uname) = Darwin; and test -x /opt/homebrew/bin/brew
+    eval (/opt/homebrew/bin/brew shellenv)
+end
+
+bind super-backspace backward-kill-line
+bind alt-backspace backward-kill-word
+
+bind super-left beginning-of-line
+bind super-right end-of-line
+
+alias python python3
+alias pip pip3
+alias ls "ls -G"
+alias ll "ls -lhG"
